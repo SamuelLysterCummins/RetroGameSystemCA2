d1350428486ae0c69bde9c5f696f0ba087644ea3
SamuelLysterCummins <20102256@mail.wit.ie>
2023-12-27 15:37:26 +0000

    .

---
+++ src/main/java/com/example/retrovideogamesystemca2/FastHash.java
@@ -0,0 +1,52 @@
+package com.example.retrovideogamesystemca2;
+import java.util.Scanner;
+
+public class FastHash<T> {
+    private HashEntry<T>[] hashTable;
+
+    public FastHash(int size) {
+        hashTable = new HashEntry[size];
+    }
+
+    static class HashEntry<T> {
+        String key;
+        T value;
+
+        HashEntry(String key, T value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public int hashFunction(String key) {
+        int hashValue = 0;
+        for (int i = 0; i < key.length(); i++) {
+            hashValue = (hashValue + key.charAt(i)) % hashTable.length;
+        }
+        return hashValue;
+    }
+
+
+    public boolean add(String key, T value) {
+        int hashIndex = hashFunction(key);
+        for (int i = 0; i < hashTable.length; i++) {
+            int index = (hashIndex + i) % hashTable.length;
+            if (hashTable[index] == null || hashTable[index].key.equals(key)) {
+                hashTable[index] = new HashEntry<>(key, value);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T get(String key) {
+        int hashIndex = hashFunction(key);
+        for (int i = 0; i < hashTable.length; i++) {
+            int index = (hashIndex + i) % hashTable.length;
+            if (hashTable[index] != null && hashTable[index].key.equals(key)) {
+                return hashTable[index].value;
+            }
+        }
+        return null;
+    }
+}
